9250c3fa77d3b1f39d3f6bd2166a3b8446aef9dc
Artur Luniaka <artur01049718@gmail.com>
2025-05-08 08:00:47 +0100

    styled banner comp for desk/finished main path of markUp

--- src/app/components/AdditionalBanner/AdditionalBanner.jsx
+++ src/app/components/AdditionalBanner/AdditionalBanner.jsx
@@ -2,9 +2,9 @@
 
 const AdditionalBanner = () => {
   return (
-    <div className="container flex flex-col my-[113px]">
-      <div>
-        <h3 className="font-raleway font-bold text-xl text-[#fff] text-center uppercase mb-5">
+    <div className="container flex flex-col my-[113px] lg:mt-[60px] lg:mb-[80px]">
+      <div className="lg:w-[447px] lg:mx-auto">
+        <h3 className="font-raleway font-bold text-xl text-[#fff] text-center uppercase mb-5 lg:text-4xl">
           Узнай, как создавать контент,который
           <br />
           <span
@@ -18,22 +18,22 @@ const AdditionalBanner = () => {
           </span>
         </h3>
       </div>
-      <p className="font-raleway font-medium text-sm text-[#fff] text-center mb-[62px]">
+      <p className="font-raleway font-medium text-sm text-[#fff] text-center mb-[62px] lg:text-2xl lg:w-[447px] lg:mx-auto lg:mb-11">
         Материалы, способные стать вирусными и охватить тысячи людей!
       </p>
-      <div className="relative w-[268px] mx-auto">
+      <div className="relative w-[268px] mx-auto lg:w-[534px]">
         <button
-          className="flex justify-center items-center w-[268px] h-[68px] rounded-[40px]"
+          className="flex justify-center items-center w-[268px] h-[68px] rounded-[40px] lg:w-[534px]"
           style={{
             boxShadow: `inset 4px 6px 10px 4px rgba(167, 93, 243, 0.2)`,
             background: `linear-gradient(121deg, #5bdbfd 0%, #7375ff 40.3%, #df93ff 79.87%, #e56f8c 100%)`,
           }}
         >
-          <span className="font-raleway font-semibold text-sm text-[#fff] mr-[29px]">
+          <span className="font-raleway font-semibold text-sm text-[#fff] mr-[29px] lg:text-[22px] lg:mr-0">
             Купить со скидкой
           </span>
         </button>
-        <span className="absolute top-0 right-0 flex justify-center items-center h-[68px] px-1.5 bg-[#ff4a77] rounded-[40px] font-raleway font-semibold text-xl text-[#fff]">
+        <span className="absolute top-0 right-0 flex justify-center items-center h-[68px] px-1.5 bg-[#ff4a77] rounded-[40px] font-raleway font-semibold text-xl text-[#fff] lg:text-2xl">
           -50%
         </span>
       </div>
--- src/app/components/MainContent/MainContent.jsx
+++ src/app/components/MainContent/MainContent.jsx
@@ -2,7 +2,6 @@
 
 import AdditionalBanner from "../AdditionalBanner/AdditionalBanner";
 import HeroContentBox from "../HeroContentBox/HeroContentBox";
-import HeroImg from "../HeroImg/HeroImg";
 import TariffsContentBox from "../TariffsContentBox/TariffsContentBox";
 
 const MainContent = () => {
@@ -13,7 +12,7 @@ const MainContent = () => {
         <TariffsContentBox />
       </div>
       <div
-        className="mb-12"
+        className="mb-12 lg:hidden"
         style={{
           background: `radial-gradient(circle at top left, rgba(100, 100, 255, 0.25), transparent 50%),
               radial-gradient(circle at bottom right, rgba(100, 200, 255, 0.2), transparent 50%),
@@ -22,6 +21,19 @@ const MainContent = () => {
           borderBottom: `2px solid`,
           borderImage: `linear-gradient(to right, #00e0ff, #a700ff, #ff2eb2) 1`,
           borderRadius: `8px`,
+        }}
+      >
+        <AdditionalBanner />
+      </div>
+      <div
+        className="hidden lg:block lg:bg-no-repeat lg:bg-center lg:bg-cover mb-[59px]"
+        style={{
+          backgroundImage: "url('/banner-bg.png')",
+          borderTop: `2px solid`,
+          borderBottom: `2px solid`,
+          borderImage: `linear-gradient(to right, #00e0ff, #a700ff, #ff2eb2) 1`,
+          borderRadius: `8px`,
+          height: `488px`,
         }}
       >
         <AdditionalBanner />
